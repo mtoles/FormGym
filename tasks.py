ab41231615455b98161adf7edb1a8bba1a949387
iiisong <isaacsong03@gmail.com>
2025-05-15 19:36:02 +0000

    add qol variables to debugger use

--- tasks.py
+++ tasks.py
@@ -43,4 +43,54 @@ class ImagePdfFill(BaseTask):
                 agent_generations_inside=marks_inside, profile_info=profile_info
             )
 
+
+        ### DEBUGGING ###
+        # break point for debugging, allows to inspect prediction results
+        # 
+        # 
+        # doc_id: document
+        # marked_fields: all fields that have predictions
+        # incorrect_fields: fields that are marked but incorrect
+        # unmapped_fields: fields that are not marked
+        # 
+        # doc_fields: all fields in the document
+        # doc_marks: all marks in the document
+        
+        doc_id = doc_state.doc_id
+        
+        doc_fields = deepcopy(doc_state.fields)
+        for field in doc_fields:
+            field.pop("id")
+            field.pop("bbox")
+            field.pop("field")
+        
+        marked_fields = list(
+            filter(
+                lambda x: x["pred"] != "",
+                doc_fields,
+            )
+        )
+        
+        incorrect_fields = list(
+            filter(
+                lambda x: x["correct"] is False,
+                marked_fields,
+            )
+        )
+        
+        doc_marks = deepcopy(doc_state.marks)
+        for mark in doc_marks:
+            mark.pop("creator")
+            mark.pop("action")
+            mark.pop("bbox")
+            
+        all_marks = set([y["pred"] for y in marked_fields])
+        unmapped_fields = list(
+            filter(
+                lambda x: x['value'] not in all_marks,
+                doc_marks,
+            )
+        )
+
+        # break point for debugging
         return doc_state
